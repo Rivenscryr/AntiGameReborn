18ee994f780f6a6cfdda8290a3f1c1191eb934d4
RiV- <ser_noche@yahoo.de>
2016-09-07 15:20:58 +0200

    ogameversionmax 6.3.0

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@ if (!AGO) {
     var AGO = {};
 }
 
-AGO.versionOGameMax = "6.2.0";
+AGO.versionOGameMax = "6.3.0";
 -1 < window.navigator.userAgent.indexOf("Firefox") ? (AGO.isFirefox = !0, AGO.isPhone = -1 < window.navigator.userAgent.indexOf("Mobile"), AGO.isTablet = -1 < window.navigator.userAgent.indexOf("Tablet"), AGO.isMobile = AGO.isPhone || AGO.isTablet
 ) : -1 < window.navigator.userAgent.indexOf("Chrome") && (AGO.isChrome = !0
 );
