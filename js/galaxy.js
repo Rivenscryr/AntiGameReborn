a4a53a1aa461a236efba8caa620ae09d34eb9021
RiV- <ser_noche@yahoo.de>
2018-08-14 18:55:00 +0200

    Fix for galaxy hotkeys

--- js/galaxy.js
+++ js/galaxy.js
@@ -5,10 +5,11 @@ AGO.Galaxy = {
         AGO.Option.is("G40") && !AGO.App.OgameMobile && (AGO.Galaxy.enabled = !0, AGO.Galaxy.improve = AGO.Option.is("G41"), AGO.Galaxy.shrink = AGO.Option.get("G42", 2), AGO.Galaxy.status = 5, AGO.Galaxy.Show(), document.getElementById("galaxytable") && AGO.Galaxy.Content()
         )
     }, onKeydown: function (a) {
-        if (document.activeElement.tagName in {'TEXTAREA':1, 'INPUT':1}) return false;
-		if (DOM.getAttribute("#galaxytable", null, "ago-keypressed", "")) return false;
-		
-		DOM.updateAttribute("#galaxytable", null, "ago-keypressed", 1, 8);
+        if (document.activeElement.tagName in {'TEXTAREA':1, 'INPUT':1}) return;
+		if (a.keyCode in {13:1, 32:1, 65:1, 68:1, 81:1, 69:1}) {
+			if (DOM.getAttribute("#galaxytable", null, "ago-keypressed", "")) return false;
+			else DOM.updateAttribute("#galaxytable", null, "ago-keypressed", 1, 8);
+		}
 		
         // Enter key pressed
         if (a.keyCode === 13 && !a.cached) {
