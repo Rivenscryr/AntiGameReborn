91916d23c24f1df075fa3d96c515c297fbafb110
RiV- <ser_noche@yahoo.de>
2019-02-26 20:53:27 +0100

    Revert "fixed bug with negative time in spy table"
    This reverts commit c6bd8423c77aa6210a6bc06aa8ed309b56f2832e.

--- js/messages.js
+++ js/messages.js
@@ -39,7 +39,6 @@ AGO.Messages = {
     },
 
     Ready: function () {
-		console.log(AGO);
         $ = "jQuery" in window ? window.jQuery : !0;
         DOM.addObserver(DOM.query('#messages .js_tabs'), {childList: true, subtree: true}, function (mutations) {
             for (var i = 0; i < mutations.length; i++) {
