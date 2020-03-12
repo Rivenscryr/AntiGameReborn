1a0539a22c2542c4e27d9a8d1bdb227c40e2961e
RiV- <ser_noche@yahoo.de>
2020-03-12 01:59:16 +0100

    Update default.js

--- docs/assets/js/default.js
+++ docs/assets/js/default.js
@@ -6,15 +6,7 @@ function offsetAnchor() {
   }
 }
 
-// Captures click events of all <a> elements with href starting with #
-/* $(document).on('click', 'a[href^="#"]', function(event) {
-  // Click events are captured before hashchanges. Timeout
-  // causes offsetAnchor to be called after the page jump.
-  window.setTimeout(function() {
-    offsetAnchor();
-  }, 0);
-}); */
-
 // Set the offset when entering page with hash present in the url
 window.setTimeout(offsetAnchor, 0);
+
 window.onhashchange = offsetAnchor;
